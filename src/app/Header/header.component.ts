24967ca191bffa0c085c3e03e95fefabb44a9ad8
IbrahimABU55 <ely.ibrahim@gmail.com>
2020-12-30 01:42:20 +0100

    adapting header based on authentication status

--- src/app/Header/header.component.ts
+++ src/app/Header/header.component.ts
@@ -1,19 +1,27 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {DataStorageService} from "../shared/services/data-storage.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit, OnDestroy{
 
+  isAuthenticated = false;
   collapsed = false;
+  userSubscription: Subscription;
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {
   }
 
   ngOnInit(): void {
+   this.userSubscription = this.authService.user.subscribe( user => {
+     this.isAuthenticated = !!user; // = !user ? false : true;
+   })
   }
 
 
@@ -23,5 +31,10 @@ export class HeaderComponent implements OnInit{
 
   fetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+
   }
 }
